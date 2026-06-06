a2b8952dea7df7d1f71a33514c69ee0d41e15dd8
Embtj <60002379+Embtj@users.noreply.github.com>
2026-06-05 19:13:35 -0500

    Implement start screen layout and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import yellowBlob from "./assets/yellow-blob.svg"
+import blueBlob from "./assets/blue-blob.svg"
+import StartScreen from './components/StartScreen'
 
 export default function App() {
+
+  
+
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <div className="app">
+      <img src={blueBlob} className="blue-blob" alt="" />
+      <img src={yellowBlob} className="yellow-blob" alt="" />
+      <div className="content">
+        <StartScreen />
+      </div> 
+    </div> 
   )
 }
---
+++ src/components/StartScreen.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function StartScreen() {
+  return (
+    <div className="container">
+      <h1 className="game-title">Quizzical</h1>
+      <p className="game-description">Some description if needed</p>
+      <button className="start-btn">Start quiz</button>
+    </div>
+  )
+}
